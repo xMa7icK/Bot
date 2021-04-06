ec6c00d593be5603d386c1faac74c9b656864ab6
Matias <77874672+xMa7icK@users.noreply.github.com>
2021-04-05 22:07:34 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+global.owner = ['529984907794'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b'
+}
+
+// Sticker WM
+global.packname = 'I want you\'EsuBot'
+global.author = 'Samu330'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
