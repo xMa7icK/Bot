40ba203b0293c3a95c1ad0aaca0cad98f2ce2f29
Matias <77874672+xMa7icK@users.noreply.github.com>
2021-05-14 10:33:54 -0400

    Update online.js

--- plugins/online.js
+++ plugins/online.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, args }) => {
 handler.help = ['here','listonline']
 handler.tags = ['group']
 handler.command = /^(here|(list)?online)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
 
-handler.admin = false
+handler.admin = true
 handler.botAdmin = false
 
 handler.fail = null
