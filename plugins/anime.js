1abe57f90fb9f792f59cc65ce5aae4bb09cb58ce
Matias <77874672+xMa7icK@users.noreply.github.com>
2021-05-14 11:34:19 -0400

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -14,7 +14,7 @@ let handler = async(m, { conn, args, command, usedPrefix }) => {
 handler.help = ['anime']
 handler.tags = ['downloader']
 handler.command = /^(anime)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
