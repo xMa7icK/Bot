58a03aea52852d0964f3e5b6c6a43f35b97b53f4
Matias <77874672+xMa7icK@users.noreply.github.com>
2021-05-14 00:25:21 -0400

    Add files via upload

---
+++ plugins/cc.js
@@ -0,0 +1,23 @@
+const axios = require('node-fetch')
+let handler = async(m, { conn }) => {
+    fetch("https://videfikri.com/api/ccgenerator/")
+  .then(res => res.json())
+  .then(batch => {
+    conn.updatePresence(m.chat, Presence.composing)
+  conn.reply(m.chat, `${batch.result.card.network}`, m)} 
+} 
+
+handler.help = ['cc']
+handler.tags = ['tools']
+handler.command = /^(cc)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+handler.limit = true
+handler.exp = 500
+module.exports = handler
